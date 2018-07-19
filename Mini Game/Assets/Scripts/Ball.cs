82477bf6cee70d6b8b15fde3b38ec5c8eb7c583e
poolofclay33 <39412974+poolofclay33@users.noreply.github.com>
2018-07-18 23:22:01 -0500

    Edit(4)
    - Got to throw the cloned balls.

--- Mini Game/Assets/Scripts/Ball.cs
+++ Mini Game/Assets/Scripts/Ball.cs
@@ -6,32 +6,33 @@ public class Ball : MonoBehaviour {
 
     public GameObject parentbone;
     public Rigidbody rigidBall;
-    public GameObject ball;
+    //public GameObject ball;
     public Transform hand;
-    private Rigidbody _instance;
 
     private void Start()
     {
-        transform.parent = parentbone.transform;
-        rigidBall.useGravity = false;
-        rigidBall.isKinematic = true;
+        //transform.parent = parentbone.transform;
+        //rigidBall.useGravity = false;
+        //rigidBall.isKinematic = true;
     }
 
     public void Release()
     {
+        Rigidbody _instance;
+
+        _instance = Instantiate(rigidBall, hand.position, hand.rotation) as Rigidbody;
+
         transform.parent = null;
         rigidBall.useGravity = true;
         rigidBall.isKinematic = false;
         transform.rotation = parentbone.transform.rotation;
-        rigidBall.AddForce(transform.forward * 20000);
+        _instance.AddForce(transform.forward * 20000);
 
-        StartCoroutine(MakeBall());
+        //StartCoroutine(MakeBall());
     }
 
     IEnumerator MakeBall()
     {
         yield return new WaitForSeconds(3);
-
-        _instance = Instantiate(rigidBall, hand.position, hand.rotation);
     }
 }
